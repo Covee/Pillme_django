3347bdb8b4f3190335711436cb2a92e46e0f1ce4
Covee <uniqtop91@gmail.com>
2018-06-09 05:25:14 +0900

    registration form 수정

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -6,16 +6,21 @@ User = get_user_model()
 
 
 class UserRegistrationForm(forms.Form):
-	username	= forms.CharField()
-	email		= forms.EmailField()
-	password1	= forms.CharField(label='비밀번호', widget=forms.PasswordInput)
-	password2	= forms.CharField(label='비밀번호 확인', widget=forms.PasswordInput)
+	username	= forms.CharField(label='아이디 ', min_length=6, max_length=20, widget= forms.TextInput
+                           (attrs={'placeholder':'4-20자 사이로'}))
+	email		= forms.EmailField(label='이메일 ', widget= forms.EmailInput
+                           (attrs={'placeholder':'이메일 입력'}))
+	password1	= forms.CharField(label='비밀번호 ', min_length=8, max_length=20, widget=forms.PasswordInput
+							(attrs={'placeholder':'8-20자 사이'}))
+	password2	= forms.CharField(label='비밀번호 확인 ', min_length=8, max_length=20, widget=forms.PasswordInput
+							(attrs={'placeholder':'8-20자 사이'}))
+
 
 	def clean_password2(self):
 		password1 = self.cleaned_data.get('password1')
 		password2 = self.cleaned_data.get('password2')
 		if password1 != password2:
-			raise ValidationError("패스워드가 일치하지 않습니다.")
+			raise forms.ValidationError("패스워드가 일치하지 않습니다.")
 		return password2
 
 	def clean_username(self):
