a0550eec55385a7e65062359130d59882571f8d9
Covee <uniqtop91@gmail.com>
2018-06-10 20:09:43 +0900

    Post admin 등록, model parameter 추가

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.models import User
+
 from django.db import models
 
 
 class Post(models.Model):
 	title		= models.CharField(max_length=50, blank=False)
+	# author		= models.ForeignKey(User)
+	content		= models.TextField(max_length=5000, blank=False, null=False)
+	files		= models.FileField(null=True, blank=True)
+	images		= models.ImageField(null=True, blank=True)
+	timestamp 	= models.DateTimeField(auto_now_add=True)
 
-	
+	# comments
+	# likes
+
+	def __str__(self):
+		return self.title
