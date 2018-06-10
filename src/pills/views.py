4b3f8a7437de8183506d465da8237d5fd3323131
Covee <uniqtop91@gmail.com>
2018-06-10 18:58:40 +0900

    검색 기능 추가, 연결

--- src/pills/views.py
+++ src/pills/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from django.views.generic import ListView, DetailView
 
 from .models import Pills
@@ -10,6 +12,16 @@ class PillListView(ListView):
 	def get_context_data(self, **kwargs):
 		context = super(PillListView, self).get_context_data(**kwargs)
 		return context
+
+	def get_queryset(self, *args, **kwargs):
+		qs = Pills.objects.all()
+		print(self.request.GET)
+		query = self.request.GET.get("q", None)
+		if query is not None:
+			qs = qs.filter(
+					Q(name__icontains=query)
+				)
+		return qs
 
 
 class PillDetailView(DetailView):
