9c44ce37d7385f164e12af744d3cf6113d36fba4
Covee <uniqtop91@gmail.com>
2018-06-25 17:06:04 +0900

    게시판 author 연결

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 
 from django.db import models
 
 
 class Post(models.Model):
 	title		= models.CharField(max_length=50, blank=False)
-	# author		= models.ForeignKey(User)
+	author		= models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False)
 	content		= models.TextField(max_length=5000, blank=False, null=False)
 	files		= models.FileField(null=True, blank=True)
 	images		= models.ImageField(null=True, blank=True)
@@ -13,6 +13,9 @@ class Post(models.Model):
 
 	# comments
 	# likes
+
+	class Meta:
+		ordering = ['-id']
 
 	def __str__(self):
 		return self.title
