be1f221397c8b19a0e1df44f4bc2f9b8b93c81d5
Covee <uniqtop91@gmail.com>
2018-06-26 15:33:09 +0900

    hitcount(조회수) for posts 1차 구현

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,15 +1,19 @@
 from django.conf import settings
-
 from django.db import models
 
+from django.contrib.contenttypes.fields import GenericRelation
+from hitcount.models import HitCount, HitCountMixin
 
-class Post(models.Model):
+
+class Post(models.Model, HitCountMixin):
 	title		= models.CharField(max_length=50, blank=False)
 	author		= models.ForeignKey(settings.AUTH_USER_MODEL, blank=False, null=False)
 	content		= models.TextField(max_length=5000, blank=False, null=False)
 	files		= models.FileField(null=True, blank=True)
 	images		= models.ImageField(null=True, blank=True)
 	timestamp 	= models.DateTimeField(auto_now_add=True)
+	hit_count 	= GenericRelation(HitCount, object_id_field='object_pk', 
+											related_query_name='hit_count_generic_relation')
 
 	# comments
 	# likes
@@ -18,7 +22,5 @@ class Post(models.Model):
 		ordering = ['-id']
 
 	def __str__(self):
-		return self.title
+		return "Post title: %s" % self.title
 
-
-	
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-from django.views.generic import TemplateView, ListView
+
+from hitcount.views import HitCountDetailView
+
+from django.views.generic import TemplateView, ListView, DetailView
 
 from .models import Post
 
@@ -13,3 +16,33 @@ from .models import Post
 class PostListView(ListView):
 	model = Post
 	template_name = 'posts/post_list.html'
+
+
+class PostMixinDetailView(object):
+	model = Post
+
+	def get_context_data(self, **kwargs):
+		context = super(PostMixinDetailView, self).get_context_data(**kwargs)
+		context['post_list'] = Post.objects.all()[:5]
+		context['post_views'] = ["ajax", "detail", "detail-with-count"]
+		return context
+
+
+# class PostDetailJSONView(PostMixinDetailView, DetailView):
+#     template_name = 'posts/post_ajax.html'
+
+#     @classmethod
+#     def as_view(cls, **initkwargs):
+#         view = super(PostDetailJSONView, cls).as_view(**initkwargs)
+#         return ensure_csrf_cookie(view)
+
+
+class PostDetailView(PostMixinDetailView, HitCountDetailView):
+    # Generic hitcount class based view.
+    pass
+
+
+class PostCountHitDetailView(HitCountDetailView):
+    model = Post        # your model goes here
+    count_hit = True    # set to True if you want it to try and count the hit
+
