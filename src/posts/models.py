9102fc76085e5507004bb94246ef9ed33e3dd02d
Covee <uniqtop91@gmail.com>
2018-06-27 00:47:31 +0900

    post create 미디어파일 경로 설정 및 템플릿에 나타나게끔 설정

--- src/posts/models.py
+++ src/posts/models.py
@@ -9,8 +9,8 @@ class Post(models.Model, HitCountMixin):
 	title		= models.CharField(max_length=50, blank=False)
 	author		= models.ForeignKey(settings.AUTH_USER_MODEL, null=False)
 	content		= models.TextField(max_length=5000, blank=False, null=False)
-	files		= models.FileField(null=True, blank=True)
-	images		= models.ImageField(null=True, blank=True)
+	files		= models.FileField(null=True, blank=True, upload_to='upload_images/posts/%Y/%m/%d')
+	images		= models.ImageField(null=True, blank=True, upload_to='upload_images/posts/%Y/%m/%d')
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 	hit_count 	= GenericRelation(HitCount, object_id_field='object_pk', 
 											related_query_name='hit_count_generic_relation')
