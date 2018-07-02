2bae2de6c398938becafc04525dfd2c72ecf70dd
Covee <uniqtop91@gmail.com>
2018-07-02 19:13:41 +0900

    good to know app 생성, settings.py, model 등록

---
+++ src/goodtoknow/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GoodtoknowConfig(AppConfig):
+    name = 'goodtoknow'
---
+++ src/goodtoknow/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+from django.contrib.contenttypes.fields import GenericRelation
+from hitcount.models import HitCount, HitCountMixin
+
+
+class Post(models.Model, HitCountMixin):
+	title		= models.CharField(max_length=50, blank=False)
+	content		= models.TextField(max_length=5000, blank=False, null=False)
+	files		= models.FileField(null=True, blank=True, upload_to='upload_images/goodtoknow/%Y/%m')
+	images		= models.ImageField(null=True, blank=True, upload_to='upload_images/goodtoknow/%Y/%m')
+	timestamp 	= models.DateTimeField(auto_now_add=True)
+	hit_count 	= GenericRelation(HitCount, object_id_field='object_pk', 
+											related_query_name='hit_count_generic_relation')
+
+	# comments
+	# likes
+
+	class Meta:
+		ordering = ['-id']
+
+	def __str__(self):
+		return self.title
+
