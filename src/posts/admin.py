634c02e25687afb3071f0a2798fc991d8368ce70
Covee <uniqtop91@gmail.com>
2018-07-11 12:19:44 +0900

    freeboard post like admin 등록

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Post, Introduce
+from .models import Post, Introduce, LikePost
 
 
 admin.site.register(Post)
-
 admin.site.register(Introduce)
+admin.site.register(LikePost)
+
+
